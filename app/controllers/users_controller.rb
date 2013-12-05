ac4d74327d0775fd728602003ea8a38e76fa1c19
faizanahmed <faizan@karuilabs.com>
2013-12-05 12:31:54 +0530

    added stylings to signin and signout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
-
-
-
-	
+	def show
+		@user=User.find(params[:id])
+		#raise @user.to_yaml
+	end
+	def edit
+		@user=User.find(params[:id])
+	end
+	def update 
+		@user=User.find(params[:id])
+		if @user.update(params[@user].permit[:name,:bio])
+			redirect_to user_path(current_user)	
+		else
+			render 'show'
+		end
+	end
 end
