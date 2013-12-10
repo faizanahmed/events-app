9b89ac43f23ce9b50e8ef5dfa797bc13f41abe23
faizanahmed <faizan@karuilabs.com>
2013-12-10 11:06:31 +0530

    Done with editing events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,9 +16,28 @@ def create
 		render 'new'
 	end
 end
+def edit
+	@user=User.find(params[:user_id])
+	@event=@user.events.find(params[:id])
+end
+def update
+	@user=User.find(params[:user_id])
+	@event=@user.events.find(params[:id])
+	@event.update(event_params)
+	redirect_to user_event_path(params[:user_id],@event)
+end
+	
+
 def show
 	@user=User.find(params[:user_id])
 	@event=@user.events.find(params[:id])
+end
+def destroy
+	@user=User.find(params[:user_id])
+	@event=@user.events.find(params[:id])
+	if @event.destroy
+		redirect_to user_event_path
+	end
 end
 def event_params
 	params.require(:event).permit(:name,:description,:avatar)
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,6 +2,13 @@
 
 class AvatarUploader < CarrierWave::Uploader::Base
 
+  include CarrierWave::MiniMagick
+
+  process :resize_to_fit => [800, 800]
+
+  version :thumb do
+    process :resize_to_fill => [200,200]
+  end
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
